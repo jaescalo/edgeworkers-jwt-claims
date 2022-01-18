aa6fdae4815678d8be3188f38ddc4f2c94d1558a
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 16:35:35 -0600

    Fix variable references

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { logger } from 'log';
 export function onClientRequest (request) {
     let jsonWebToken = request.getHeader('jwt');
     //let base64Payload = jsonWebToken.split('.')[1];
-    const jsonWebToken = jwt.split('.');
+    const jwtParts = jsonWebToken.split('.');
     const payloadInBase64UrlFormat = jwtParts[1];
     logger.log(payloadInBase64UrlFormat);
 }
