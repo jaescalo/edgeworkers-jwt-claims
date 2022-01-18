b84e6890bcc3483697f1e5f15026c89887d412de
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 15:57:49 -0600

    Removed split function

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ import { logger } from 'log';
 
 export function onClientRequest (request) {
     let jsonWebToken = request.getHeader('jwt');
-    let base64Payload = jsonWebToken.split('.')[1];
-    logger.log(base64Payload);
+    logger.log(jsonWebToken);
 }
