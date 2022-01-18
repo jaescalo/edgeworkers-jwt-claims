079b7df94d0c4342f6e07bf551b987e73c52dbbf
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 16:50:01 -0600

    Test jwt in header vs inline

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { logger } from 'log';
 
 export function onClientRequest (request) {
-    //let jsonWebToken = request.getHeader('jwt');
+    const jwtHeader = request.getHeader('jwt');
+    logger.log(jwtHeader);
+
     const jsonWebToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ";
-    logger.log(jsonWebToken);
-    //let base64Payload = jsonWebToken.split('.')[1];
-    const jwtParts = jsonWebToken.split('.');
-    const payloadInBase64UrlFormat = jwtParts[1];
-    logger.log(payloadInBase64UrlFormat);
+    
+    let jwtBase64Payload = jsonWebToken.split('.')[1];
+    logger.log(jwtBase64Payload);
 }
