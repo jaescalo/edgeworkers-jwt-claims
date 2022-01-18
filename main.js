335f8a38e1a8e2bf1620d3a2db171023c0ca76df
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 15:51:50 -0600

    Fix export

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 import { logger } from 'log';
 
-function onClientRequest (request) {
+export function onClientRequest (request) {
     let jsonWebToken = request.getHeader('jwt');
     let base64Payload = jsonWebToken.split('.')[1];
     logger.log(base64Payload);
-
 }
-
-export { onClientRequest };
