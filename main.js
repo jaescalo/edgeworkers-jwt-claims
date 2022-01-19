1d8d965b41f287fee1c0b61b0a69681c4d2591e4
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 18:16:43 -0600

    Stringify jwt header

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ import { logger } from 'log';
 
 export function onClientRequest (request) {
     const jwtHeader = request.getHeader('jwt');
-    logger.log(jwtHeader);
-    logger.log(typeof(jwtHeader));
+    const jwtHeaderString = JSON.stringify(jwtHeader);
+    logger.log(jwtHeaderString);
+    logger.log(typeof(jwtHeaderString));
 
     const jsonWebToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ";
     
