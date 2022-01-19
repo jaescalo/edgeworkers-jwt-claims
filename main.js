e3c0b8d72c17c95114d3a834f9b832e560d24ca3
Jaime Escalona <jaescalo@akamai.com>
2022-01-18 18:26:20 -0600

    Remove typeof

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ export function onClientRequest (request) {
     const jwtHeader = request.getHeader('jwt');
     const jwtHeaderString = JSON.stringify(jwtHeader);
     logger.log(jwtHeaderString);
-    logger.log(typeof(jwtHeaderString));
     
     let jwtBase64Payload = jwtHeaderString.split('.')[1];
-    logger.log(typeof(jwtBase64Payload));
+    logger.log(jwtBase64Payload);
 }
